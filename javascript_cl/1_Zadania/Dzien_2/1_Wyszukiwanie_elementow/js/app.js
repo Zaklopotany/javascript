4f72da4c7528949f0017559b4d8733a3995f38dd
mzukowski <mzukowski@gmail.com>
2018-07-03 15:21:29 +0200

    dzien2

--- javascript_cl/1_Zadania/Dzien_2/1_Wyszukiwanie_elementow/js/app.js
+++ javascript_cl/1_Zadania/Dzien_2/1_Wyszukiwanie_elementow/js/app.js
@@ -1,6 +1,16 @@
 //Co oznaca poniższy kod dowiecie się w dalszej części modułu
 document.addEventListener("DOMContentLoaded", function(){
+  var ids = document.getElementsByTagName("h1");
+  console.log("liczba h1 " + ids.length);
+
+  var classes = document.getElementsByClassName("offers");
+  for(i = 0; i<classes.length; i++) {
+    console.log(classes[i]);
+  }
+  var dives = document.
+
     /*
+
     Poniżej napisz kod rozwiązujący zadania
      */
 });
