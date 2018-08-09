a58ec036f809e33a0046ccdd3cccd1f3fb214b43
zaklopotany <marry.jane.kelly@gmail.com>
2018-08-09 20:59:57 +0200

    zadnaie

--- javascript_cl/1_Zadania/Dzien_3/4_Inputy_i_formularze/js/zadanie03.js
+++ javascript_cl/1_Zadania/Dzien_3/4_Inputy_i_formularze/js/zadanie03.js
@@ -1,30 +1,62 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var checkboxes = (Array.from(document.querySelectorAll("input"))).filter(function(e) {
+  var checkboxes = (Array.from(document.querySelectorAll("input")));
+  var inputs = checkboxes.filter(function(e) {
     return e.dataset.price > 0;
   });
 
-
-checkboxes.forEach(function(e) {
-  e.addEventListener("click", function() {
+  //custom action to manage input event
+  function myAction() {
     var price = this.dataset.price;
-    var totalSum = document.querySelector("#price").innerHTML;
+    var totalSumPLN = document.querySelector("#price");
+    var totalReg = new RegExp("^[0-9]+[.][0-9]+|^[0-9]+");
+    var getTotal = totalReg.exec(totalSumPLN.innerHTML);
 
-
-    var totalReg = new RegExp("^[0-9]]+[.][0-9]+|^[0-9]+");
-    var getTotal = totalReg.exec(totalSum)
-    console.log(totalReg.exec(price));
-    console.log(totalSum);
-    console.log(getTotal);
-    console.log(this.dataset.price);
-    if(e.checked) {
-      // console.log(price);
-
+    if (this.checked) {
+      totalSumPLN.innerHTML = parseFloat((+getTotal + +price)).toFixed(2) + "zł";
     } else {
+      totalSumPLN.innerHTML = parseFloat((+getTotal - price)).toFixed(2) + "zł";
 
     }
+  };
+
+  //selectALL EVENT
+  checkboxes[0].addEventListener('change', function() {
+    if (this.checked) {
+      inputs.forEach(function(e) {
+        if (!e.checked) {
+          e.checked = true;
+          e.dispatchEvent(new Event('change'));
+        }
+      })
+      this.checked = false;
+    }
   })
-})
+
+  //DESELECTALL EVENT
+  checkboxes[checkboxes.length - 1].addEventListener('change', function() {
+    if (this.checked) {
+      inputs.forEach(function(e) {
+        if (e.checked) {
+          e.checked = false;
+          e.dispatchEvent(new Event('change'));
+        }
+      })
+      this.checked = false;
+    }
+  })
+
+  //SINGLE CHECKBOX EVENT
+  checkboxes.filter(function(e) {
+    return e.dataset.price > 0;
+  }).forEach(function(e) {
+    e.addEventListener('change', myAction);
+  })
+  //submit event
+  document.querySelector("button").addEventListener('click', function() {
+    alert("totalPrice is " + document.querySelector("#price").innerHTML);
+  })
+
 
 
 
---
+++ javascript_cl/1_Zadania/Dzien_3/4_Inputy_i_formularze/js/zadanie04.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var select = document.querySelector('select');
+  var button = document.querySelector('button');
+
+  button.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelectorAll('img').forEach(function(e, i) {
+      switch (select.selectedIndex) {
+        case 0:
+          hideImages(e, "windows")
+          console.log("co to ");
+          break;
+        case 1:
+          hideImages(e, "apple");
+          break;
+        case 2:
+          hideImages(e, "ubuntu");
+          break;
+
+      }
+    })
+  })
+
+
+
+  function hideImages(image, name) {
+    if (image.src.includes(name)) {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
+  }
+
+
+})
